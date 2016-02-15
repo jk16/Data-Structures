1e2277c2e859733033f82a1fab90affc4fe77324
jk16 <dude16kh@gmail.com>
2016-02-15 14:17:15 -0500

    testing

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5,6,7,8,9};
+        int[] a = {0,1,2,3,4,5,6,7,8,9};
         QuickUnionUF q = new QuickUnionUF(a.length);
-        System.out.println(q.toString());
-//        System.out.println(q.root(1));
+        for(int i=0; i<a.length-1; i++) {
+        	q.union(i, i+1);
+        }
+        q.union(a.length-2, a.length-1);
     }
 }
