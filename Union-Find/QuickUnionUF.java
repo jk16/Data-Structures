d7a8a1edbe7771424aec086a228bcbc30a63f62f
jk16 <dude16kh@gmail.com>
2016-02-14 19:14:43 -0500

    explanation and implemented class/methods

---
+++ Union-Find/QuickUnionUF.java
@@ -0,0 +1,48 @@
+/*
+Data Structure: http://imgur.com/v2YPmxO
+    * integer array id[] of size N
+    * interpretation: id[i] is parent of i
+
+Find --> check if p and q are connected: they must have the same root
+Union --> Merge components containing p and q: set id (ie tree) of q to id of p (ie tree)
+
+Methods:
+QuickUnionUF(int N) : initialize the array and have the nodes point to themselves
+int root(int i): chase parent pointers until reach root
+boolean connected(int p, int q): if they have the same parent then they are equal
+void union(int p, int q): root of p point to root of q
+
+
+*/
+
+public QuickUnionUF
+{
+        private int[] id;
+
+        public QuickUnionUF(int N) 
+        {
+            id = new int[N];
+            for (int i=0; i<N; i++) {
+                //set id to itself
+                id[i] = i;
+            }
+        }
+
+        private int root(int i) {
+            //
+            while(i != id[i]) {
+                i = id[i];
+            }
+            return i;
+        }
+
+        public boolean connected(int p, int q) {
+            return root(p) == root(q);
+        }
+
+        public void union(int p, int q) {
+            int i = root(p);
+            int j = root(q);
+            id[i] = j;
+        }
+}
