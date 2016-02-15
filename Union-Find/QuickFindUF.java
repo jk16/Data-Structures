b0febedd9adc7759149a6120e83f6fa1a4632ba8
jk16 <dude16kh@gmail.com>
2016-02-14 19:14:26 -0500

    made it clearer

--- Union-Find/QuickFindUF.java
+++ Union-Find/QuickFindUF.java
@@ -28,7 +28,6 @@ id[] = [0,1,1,8,8,0,0,1,8,8]
 * 1,2,7 connected
 * 3,4,8,9 connected
 
-Methods:
 Find --> check if p and q are connected
     * id[6] =0, id[1] = 1 --> id[6] == id[1] --> false
 
@@ -37,7 +36,11 @@ Union --> Merge components containing p and q by changing entries whose id equal
     * Union(6,1): p=6 and q=1
         * since p=6 we need to change the connected components of 6 ie {0,5,6}
             * [0,1,1,8,8,0,0,1,8,8] --> [1,1,1,8,8,1,1,1,8,8]
-
+Methods:
+boolean connected(int p, int q): returns True if they are connected false if they are not
+void union(int p, int q): for each id: 
+                            if that ID equals pID
+                                set that ID to qID
 */
 
 public class QuickFindUF 
