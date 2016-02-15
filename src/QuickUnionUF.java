5218f04c699a3f1f14e1a6e1f69c0e7ae94866a4
jk16 <dude16kh@gmail.com>
2016-02-15 14:17:08 -0500

    made comments clearer, added some print statements

--- src/QuickUnionUF.java
+++ src/QuickUnionUF.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
 Data Structure: http://imgur.com/v2YPmxO
     * integer array id[] of size N
@@ -7,10 +9,10 @@ Find --> check if p and q are connected: they must have the same root
 Union --> Merge components containing p and q: set id (ie tree) of q to id of p (ie tree)
 
 Methods:
-QuickUnionUF(int N) : initialize the array and have the nodes point to themselves
-int root(int i): chase parent pointers until reach root
-boolean connected(int p, int q): if they have the same parent then they are equal
-void union(int p, int q): root of p point to root of q
+* QuickUnionUF(int N) : initialize the array and have the nodes point to themselves
+* int root(int i): chase parent pointers until reach root
+* boolean connected(int p, int q): if they have the same parent then they are equal
+* void union(int p, int q): root of p point to root of q
 
 
 */
@@ -28,9 +30,10 @@ public class QuickUnionUF
             }
         }
 
-        private int root(int i) {
+        public int root(int i) {
             //
             while(i != id[i]) {
+            	System.out.println("The parent of: " + i + " is: " +  id[i]);
                 i = id[i];
             }
             return i;
@@ -44,5 +47,9 @@ public class QuickUnionUF
             int i = root(p);
             int j = root(q);
             id[i] = j;
+        }
+        
+        public String getID() {
+        	return Arrays.toString(id);
         }
 }
