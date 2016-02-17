1a010f0935b43d7556f068c4f12ce5b656a5e63e
jk16 <dude16kh@gmail.com>
2016-02-16 20:29:56 -0500

    added top/bot virtual nodes and skeleton for isFull

--- src/Percolation.java
+++ src/Percolation.java
@@ -5,17 +5,26 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 	private WeightedQuickUnionUF grid;
-	private int N;
+	private int N, vBot, vTop;
 	public Percolation(int N)
 	{
 //		create N-by-N grid, with all sites blocked
-		grid = new WeightedQuickUnionUF(N);
+//		N+2 is to have the top/bottom virtual nodes
+//			N: Top and N+1: bottom
+		
+		grid = new WeightedQuickUnionUF(N+2);
+		vTop = N;
+		vBot = N+1;
+		for (int i=1; i<=N; i++) {
+			grid.union(vTop, i);
+			grid.union(i, vBot);
+		}
 		
 	}
 	
 	public void open(int i, int j) {   
 //	open site (row i, column j) if it is not open already
-//		if is not open
+//		if is not open (use isOpen method)
 //			determine how many adjacent nodes it has
 //			based on number of adjacent Nodes, use union those adj nodes
 //	    else return
@@ -30,6 +39,12 @@ public class Percolation {
 //				return true
 		return false;
 	}
+	
+	public boolean isFull(int i, int j) {
+		// is site (row i, column j) full?
+		//the site is full when virtual nodes are connected
+		return false;
+	} 
 	
 	public List<Integer> getAdjNodes(int cNode) {
 		List<Integer> adjNodes = new ArrayList<Integer>();
@@ -71,8 +86,9 @@ public class Percolation {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Percolation p = new Percolation(5);
+		Percolation p = new Percolation(5); //go from (0,N-1) N: Top, N+1 will be bottom
 		WeightedQuickUnionUF g = p.getGrid();
+		System.out.println(g.connected(0, 4));
 	}
 
 }
