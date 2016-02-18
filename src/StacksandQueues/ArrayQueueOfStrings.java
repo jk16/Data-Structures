dd9337e8003cfaf3d5c518bae4189a07f74b0c36
jk16 <dude16kh@gmail.com>
2016-02-18 17:20:15 -0500

    ArrayQueueOfStrings implementation

---
+++ src/StacksandQueues/ArrayQueueOfStrings.java
@@ -0,0 +1,23 @@
+package StacksandQueues;
+
+public class ArrayQueueOfStrings {
+	String[] q;
+	int tail, head;
+	
+	public ArrayQueueOfStrings(int capacity) 
+	{
+		q = new String[1];
+	}
+	
+	public void enqueue(String item) {
+//		if items in q are same size of array
+//			double size of array
+		
+		
+	}
+	
+	public String dequeue() {
+		
+	}
+	
+}
