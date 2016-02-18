04e2290471bbbfe9160896935e537eb0c30ad8bd
jk16 <dude16kh@gmail.com>
2016-02-17 20:26:05 -0500

    when array is 1/4 full half it, used resize method for that

--- src/StacksandQueues/FixedCapacityStackOfStrings.java
+++ src/StacksandQueues/FixedCapacityStackOfStrings.java
@@ -26,6 +26,18 @@ public class FixedCapacityStackOfStrings {
 //		fix:
 		String item = s[--N];
 		s[N] = null;
+//		halve size of array when array is 1/4 full
+		if (N>0 && N == s.length/4) 
+			resize(s.length/2);
 		return item;
+	}
+	
+	public void resize(int capacity) {
+		//create new array of that capacity
+		String[] copy = new String[capacity];
+		for(int i=0; i<N; i++) 
+			//copy stack onto this array
+			copy[i] = s[i];
+		s = copy;
 	}
 }
